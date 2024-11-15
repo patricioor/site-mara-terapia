d7d615d307745f847dd77ac150d8fe3d57ccb229
Tico <patricio.osterno1@gmail.com>
2024-11-15 12:32:34 -0300

    Inserido mais um component sob demanda do cliente: about-me. Realizado alteracoes secao about no component about:excluido texto anterior e substituido por outro e retirada a imagem e inserido um video apresentacao

---
+++ src/app/components/about-me/about-me.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {FooterComponent} from "../../shared/footer/footer.component";
+import {HeaderComponent} from "../../shared/header/header.component";
+import {WhatsappButtonComponent} from "../../shared/whatsapp-button/whatsapp-button.component";
+
+@Component({
+  selector: 'app-about-me',
+  standalone: true,
+    imports: [
+        FooterComponent,
+        HeaderComponent,
+        WhatsappButtonComponent
+    ],
+  templateUrl: './about-me.component.html',
+  styleUrl: './about-me.component.scss'
+})
+export class AboutMeComponent {
+
+}
